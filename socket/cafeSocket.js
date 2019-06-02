dd62926acf112ed29a5edfe566a27952bd73230e
whatsupMate <51185184+whatsupMate@users.noreply.github.com>
2019-06-02 17:33:46 -0300

    Update cafeSocket.js

--- socket/cafeSocket.js
+++ socket/cafeSocket.js
@@ -4,19 +4,6 @@ const User = require('../model/User')
 var discussions = []
 var topicId = -1;
 
-
-function markdownToHTML(md)
-{
-    return md
-        .replace(/\*\*(.+?)\*\*/g, "<b>$1</b>") // **Bold**
-        .replace(/__(.+?)__/g, "<u>$1</u>") // __Underline__
-        .replace(/\*(.+?)\*/g, "<i>$1</i>") // *Italic*
-        .replace(/_(.+?)_/g, "<i>$1</i>") // _Italic_
-        .replace(/^> ?(.*)/gm, "<font size='10' color='#5f779D'>$1</font>") // > Quote
-        .replace(/!\[(.+?)\]\((.+?)\)/g, "<img src=\"$1\" title=\"$2\"/>") // ![Image description](URL)
-        .replace(/\n/g, "<br />"); // Break line to <br>
-}
-
 module.exports = (io) => {
     const cafeSocket = io.of('/cafe');
     cafeSocket.on('connection', function(socket){
@@ -37,15 +24,15 @@ module.exports = (io) => {
             if (data.topic.title.length == 0 || data.topic.title.length > 24) isValid = false; 
             if (data.topic.content[0].content.length == 0 || data.topic.content[0].content.length > 200) isValid = false; 
             const verify = jwt.verify(data.token, process.env.TOKEN_SECRET, function (err, decoded) {
-                if(err == null) { return true } else { isValid = false};
+                if(err == null) { return true } else { isValid = false}
             })
 
             if(isValid) {
-                topicId++;
+                topicId++
                 const user = await User.findOne({_id: data.userId});
-                discussions.push({id: topicId, title: data.topic.title, owner: user.username, avatar: user.avatar, content: [{topicID: data.topicId, id: 0, content: markdownToHTML(data.topic.content[0].content), owner: user.username, avatar: user.avatar, colorName: user.colorName}], lastComment: data.topic.lastComment, date: data.topic.date})
-                cafeSocket.emit("updateDiscussions", discussions);
-                socket.emit("openTopic", discussions[topicId]);
+                discussions.push({id: topicId, title: data.topic.title, owner: user.username, avatar: user.avatar, content: [{topicID: topicId, id: 0, content: data.topic.content[0].content, owner: user.username, avatar: user.avatar, colorName: user.colorName}], lastComment: data.topic.lastComment, date: data.topic.date})
+                cafeSocket.emit("updateDiscussions", discussions)
+                socket.emit("openTopic", discussions[topicId])
             } 
         })
 
@@ -55,10 +42,14 @@ module.exports = (io) => {
 
         socket.on("newComment", async (data) => {
             const user = await User.findOne({_id: data.owner});
-            discussions[data.topicId].lastComment = new Date();
-            discussions[data.topicId].content.push({topicID: data.topicId, id: discussions[data.topicId].content[discussions[data.topicId].content.length - 1].id + 1, content: markdownToHTML(data.content), owner: user.username, avatar: user.avatar, colorName: user.colorName})
-            socket.emit("openTopic", discussions[data.topicId]);
-            cafeSocket.emit("updateDiscussions", discussions);
+            discussions[data.topicId].lastComment = new Date()
+            discussions[data.topicId].content.push({topicID: data.topicId, id: discussions[data.topicId].content[discussions[data.topicId].content.length - 1].id + 1, content: data.content, owner: user.username, avatar: user.avatar, colorName: user.colorName})
+            socket.emit("openTopic", discussions[data.topicId])
+            cafeSocket.emit("updateDiscussions", discussions)
+        })
+
+        socket.on("getContent", (data) => {
+            socket.emit("quote", discussions[data.topicID].content[data.id])
         })
     });
 }
